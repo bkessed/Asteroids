d6ad35b559eae5ce7b27f829e33d15f4216aa2a4
root <root@LAPTOP-B7KDCPRG.localdomain>
2025-07-09 18:56:51 +0300

    Player

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
 import pygame
 from constants import *
+from player import *
+from circleshape import *
 
 def main():
     pygame.init()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
-        screen.fill("black")
-        pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        dt = clock.tick(60) / 1000
+        screen.fill("black")
+        updatable.update(dt)
+        for drawe in drawable:
+            drawe.draw(screen)
+        pygame.display.flip()
             
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
